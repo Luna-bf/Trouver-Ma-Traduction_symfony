7cc492eb8cf05041dbf24b909e4ab3fc39f502bf
Luna-bf <lburattifayolle@gmail.com>
2026-01-21 15:40:09 +0100

    Ajout des différentes pages grâce aux Routes

--- src/Controller/TranslationController.php
+++ src/Controller/TranslationController.php
@@ -11,9 +11,49 @@ use Symfony\Component\Routing\Attribute\Route;
 final class TranslationController extends AbstractController
 {
     #[Route('', name: 'landing')]
-    public function index(): Response
+    public function landing(): Response
     {
-        return $this->render('translation/index.html.twig', [
+        return $this->render('translation/landing.html.twig', [
+            'controller_name' => 'TranslationController',
+        ]);
+    }
+
+    #[Route('form/register', name: 'register')]
+    public function register(): Response
+    {
+        return $this->render('translation/form/register.html.twig', [
+            'controller_name' => 'TranslationController',
+        ]);
+    }
+
+    #[Route('form/login', name: 'login')]
+    public function login(): Response
+    {
+        return $this->render('translation/form/login.html.twig', [
+            'controller_name' => 'TranslationController',
+        ]);
+    }
+
+    #[Route('home', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('translation/home/home.html.twig', [
+            'controller_name' => 'TranslationController',
+        ]);
+    }
+
+    #[Route('home/search_result', name: 'search_result')]
+    public function searchResult(): Response
+    {
+        return $this->render('translation/home/searchResult.html.twig', [
+            'controller_name' => 'TranslationController',
+        ]);
+    }
+
+    #[Route('file/file_viewer', name: 'file_viewer')]
+    public function fileViewer(): Response
+    {
+        return $this->render('translation/file/fileViewer.html.twig', [
             'controller_name' => 'TranslationController',
         ]);
     }
