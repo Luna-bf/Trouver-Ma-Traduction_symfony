4dc8974284ec7f2aea263e2feb0a6324696c55cc
Luna-bf <lburattifayolle@gmail.com>
2026-01-26 14:40:05 +0100

    Ajout d'une logique pour envoyer un fichier pdf de façon sécurisée (Symfony)

--- src/Controller/TranslationController.php
+++ src/Controller/TranslationController.php
@@ -7,9 +7,12 @@ use App\Form\TranslationType;
 use App\Repository\TranslationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('', name: 'translation_')]
 final class TranslationController extends AbstractController
@@ -54,8 +57,9 @@ final class TranslationController extends AbstractController
         ]);
     }
 
+    // Pour l'envoi de fichiers PDF : https://stackoverflow.com/questions/43001978/upload-pdf-file-with-symfony
     #[Route('/posts/new_upload', name: 'new_upload')]
-    public function newUpload(Request $request, EntityManagerInterface $em): Response
+    public function newUpload(Request $request, EntityManagerInterface $em, SluggerInterface $slugger, #[Autowire('%kernel.project_dir%/public/uploads/translations')] string $translationsDirectory): Response
     {
         // Création d'une nouvelle instance de Translation
         $translation = new Translation();
@@ -69,10 +73,29 @@ final class TranslationController extends AbstractController
         // Vérifie si le formulaire est valide
         if ($translationForm->isSubmitted() && $translationForm->isValid()) {
 
+            $translationFile = $translationForm->get('translationFileName')->getData();
+
             $fullTranslation = $translationForm->getData(); // revoir à quoi ça sert
             $fullTranslation->setCreatedAt(new \DateTimeImmutable()); // enregistre la date du jour
             $user = $this->getUser(); // récupère les données l'utilisateur connecté
             $fullTranslation->setUser($user); // enregistre les données de l'utilisateur connecté
+
+            if ($translationFile) {
+                $originalFilename = pathinfo($translationFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $translationFile->guessExtension();
+
+                // Move the file to the directory where translations are stored
+                try {
+                    $translationFile->move($translationFile, $newFilename);
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'translationFilename' property to store the PDF file name instead of its contents
+                $fullTranslation->setTranslationFilename($newFilename);
+            }
 
             $em->persist($fullTranslation); // Prépare la requête
             $em->flush(); // Exécute la requête
@@ -89,7 +112,7 @@ final class TranslationController extends AbstractController
 
 
     #[Route('translation/posts/edit/{id}', name: 'edit')]
-    public function edit(Translation $translation, Request $request, EntityManagerInterface $em): Response
+    public function edit(Translation $translation, Request $request, EntityManagerInterface $em, SluggerInterface $slugger, #[Autowire('%kernel.project_dir%/public/uploads/translations')] string $translationsDirectory): Response
     {
         // Initialisation du formulaire
         $translationForm = $this->createForm(TranslationType::class, $translation);
@@ -99,6 +122,26 @@ final class TranslationController extends AbstractController
 
         // Vérifie si le formulaire est valide
         if ($translationForm->isSubmitted() && $translationForm->isValid()) {
+
+            $translationFile = $translationForm->get('translationFileName')->getData();
+
+            // Si un fichier est envoyé dans le formulaire
+            if ($translationFile) {
+                $originalFilename = pathinfo($translationFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $translationFile->guessExtension();
+
+                // Move the file to the directory where translations are stored
+                try {
+                    $translationFile->move($translationFile, $newFilename);
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'translationFilename' property to store the PDF file name instead of its contents
+                $translation->setTranslationFilename($newFilename);
+            }
 
             $em->persist($translation);
             $em->flush();
