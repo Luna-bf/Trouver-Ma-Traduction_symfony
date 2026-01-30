b584b3adb3c3f000ff3a4950f807d8d73cc07353
Luna-bf <lburattifayolle@gmail.com>
2026-01-30 13:26:46 +0100

    Mise à jour des messages d'erreur du formulaire

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -85,11 +85,11 @@ class RegistrationFormType extends AbstractType
                 ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez définir un mot de passe.',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
