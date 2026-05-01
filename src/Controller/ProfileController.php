56461123ad656384f81568f440bd561a715f952f
Luna-bf <lburattifayolle@gmail.com>
2026-05-01 09:50:50 +0200

    Ajout d'une méthode profile(), qui affiche les informations associées au profil

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,24 +3,61 @@
 namespace App\Controller;
 
 use App\Entity\Profile;
+use App\Entity\User;
 use App\Form\ProfileType;
+use App\Repository\TranslationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 #[Route('/profile', name: 'profile_')]
 final class ProfileController extends AbstractController
 {
     #[Route('', name: 'create')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
         $profile = new Profile();
+        $user = $this->getUser();
 
         $profileForm = $this->createForm(ProfileType::class, $profile);
 
-        return $this->render('profile/index.html.twig', [
+        $profileForm->handleRequest($request);
+
+        if($profileForm->isSubmitted() && $profileForm->isValid()) {
+
+            $fullProfile = $profileForm->getData();
+            $fullProfile->setUser($user);
+
+            $em->persist($profile);
+            $em->flush();
+
+            return $this->redirectToRoute('translation_home');
+        }
+
+        return $this->render('profile/form/create.html.twig', [
             'profileForm' => $profileForm,
+        ]);
+    }
+
+    #[Route('/myProfile', name: 'show')]
+    public function profile(#[CurrentUser] User $user, TranslationRepository $repo): Response
+    {
+        $user_id = $user->getId(); // Récupère l'identifiant de l'utilisateur actuellement connecté
+        $translations = $repo->findBy(['user' => $user_id]); // Je récupère les traductions associées à l'utilisateur
+        $profile = $user->getProfile(); // Récupère le nom de l'utilisateur à partir de l'entité User (ici, l'utilisateur actuellement connecté)
+        $message = "";
+
+        if ($translations === []) {
+            $message = "Vous n'avez aucune traduction.";
+        }
+
+        return $this->render('profile/index.html.twig', [
+            'translations' => $translations,
+            'profile' => $profile,
+            'message' => $message
         ]);
     }
 }
